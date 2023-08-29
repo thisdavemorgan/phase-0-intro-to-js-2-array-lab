856d824fa110abdb3da51a474629fdb2a9f525a1
Emily Valdez <emily.milan.valdez@gmail.com>
2023-08-29 11:22:47 -0500

    arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+ return cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+}
+function appendCat(name) {
+return [...cats, "Broom"];
+}
+function prependCat(name) {
+    return ["Arnold", ...cats];
+    }
+ function removeLastCat() {
+    return cats.slice(0,-1);
+ }   
+ function removeFirstCat() {
+    return cats.slice(1);
+ }
